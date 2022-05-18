695e70bc6bd7345a68b7150eef9bcec5cf043460
E116324 <E116324@localhost>
2022-05-18 13:14:56 +1000

    My second commit.

--- LectorialSession/src/Student.java
+++ LectorialSession/src/Student.java
@@ -3,6 +3,11 @@ public class Student {
 
 	private int ID;
 	private String full_name;
+	public Student (int studentID, String stundetFull_Name)
+	{
+		ID= studentID;
+		full_name=stundetFull_Name;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
